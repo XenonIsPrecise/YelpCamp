22b0877bde7a14b1f614956f24126bf9858ce58c
Sujal Acharya <sujalacharya567@gmail.com>
2024-01-30 16:24:47 -0500

    Creating Template

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Database connected");
+});
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,4 +27,10 @@ app.listen(3000, function(){
 
 app.get('/home',(req,res)=>{
     res.render('home');
+})
+
+app.get('/makecampground',async(req,res)=>{
+    const camp = await new Campground({title:'My Backyard',description:'cheap camping'});
+    camp.save();
+    res.send(camp);
 })
