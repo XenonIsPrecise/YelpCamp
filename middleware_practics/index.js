712fc8cd33b3b512185134d856de94a72bf2d1c0
Sujal Acharya <sujalacharya567@gmail.com>
2024-01-31 23:03:53 -0500

    Middleware complete

--- middleware_practics/index.js
+++ middleware_practics/index.js
@@ -3,13 +3,35 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('tiny'));
+app.use((req,res,next)=>{
+    req.requestTime = Date.now();   
+    next();
+})
+
+const verifyPassword = (req,res,next)=>{
+    const {password} = req.query;
+    if(password === "sujal"){
+        next();
+    }else{
+        res.send("Sorry you need a password to continue.")
+    }
+}
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
 app.get('/dogs',(req,res)=>{
+    console.log(`Date and Time: ${req.requestTime}`)
     res.send("I am a dog. Woff Woof!!")
+})
+
+app.get('/secret', verifyPassword,(req,res)=>{
+    res.send("Although I talk about being mature but to be honest i am still a baby on the inside.")
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not found")
 })
 
 app.listen(3300,()=>{
